e3f19f77266a5f0fcbfff466bc9ca9c89947e138
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2020-12-13 06:55:41 +0200

    Add properly typed navigation stack with 2 screens

--- src/List/index.tsx
+++ src/List/index.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../App";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+type ProfileScreenNavigationProp = StackNavigationProp<
+	RootStackParamList,
+	"Home"
+>;
+
+type Props = {
+	navigation: ProfileScreenNavigationProp;
+};
+
+export default function App(props: Props) {
+	const { navigation } = props;
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<Text>Screen 1 : List Component</Text>
+			<Button
+				onPress={() => navigation.navigate("Details", { id: "1" })}
+				title="Go to details page"
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
