1b7b59acb8ee7d54b06e972674397cdf23ff2e4c
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2020-12-13 11:18:52 +0200

    Group all List types under types.ts file

---
+++ src/List/types.ts
@@ -0,0 +1,37 @@
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../App';
+
+export type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+
+export interface NavigationProp {
+    navigation: ProfileScreenNavigationProp;
+}
+
+export interface Character {
+    id: string;
+    name: string;
+    image: string;
+}
+
+export interface Info {
+    count: number;
+    pages: number;
+    next: number;
+    prev: number;
+}
+export interface Response {
+    characters: {
+        info: Info;
+        results: [Character];
+    };
+}
+
+export interface Variables {
+    name?: string;
+    page?: string;
+}
+
+export interface InputProps {
+    name?: string;
+    page?: string;
+}
