3973dd8794053ffdbc8581cd0c74d9eedc9f194f
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2020-12-13 11:20:29 +0200

    Refactor HOC into Hooks and pass simpler props to child list component.

--- src/List/index.tsx
+++ src/List/index.tsx
@@ -1,63 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { gql } from "@apollo/client";
-import { ChildDataProps, graphql } from "@apollo/react-hoc";
-import List from './list'
+import { useQuery } from '@apollo/client';
+import List from './list';
+import { Character, NavigationProp, InputProps, Variables, Response } from './types';
+import { CHARACTERS_QUERY } from './queries';
 
 //characters(page: 2, filter: { name: "rick" }) {
 
-const CHARACTERS_QUERY = gql`
-query {
-  characters{
-    info {
-      next
-      prev
-      pages
-      count
+const Loading = () => <Text>Loading...</Text>;
+const Error = () => <Text>Error!</Text>;
+
+export const ListContainer = (props: InputProps & NavigationProp) => {
+    const { loading, error, data, refetch } = useQuery<Response, Variables>(CHARACTERS_QUERY, {
+        variables: { name: props.name },
+    });
+    if (loading) {
+        return <Loading />;
     }
-    results {
-      name
-      image
+    if (error) {
+        return <Error />;
     }
-  }
-}
-`;
-
-interface Character {
-  name: string;
-  image: string;
+    return <List {...props} characters={data && data.characters && data.characters.results} />;
 };
 
-interface Info {
-	count: number;
-	pages: number;
-	next: number;
-	prev:number;
-}
-interface Response {
-	characters: {
-		info: Info;
-		results: [Character];
-	}
-};
-
-interface Variables {
-  name?: string;
-  page?: string;
-};
-
-interface InputProps {
-  name?: string;
-  page?: string;
-};
-type ChildProps = ChildDataProps<InputProps, Response, Variables>;
-
-
-export const withCharacters = graphql<InputProps, Response, Variables, ChildProps>(CHARACTERS_QUERY, {
-  options: ({ name, page }) => ({
-    variables: { name, page }
-  })
-});
-
-export default withCharacters(List)
-
+export default ListContainer;
