a96fa2145066a6ded0ce152fd9343f4ec51f4b3a
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2020-12-13 07:41:06 +0200

    Add and configure eslint/prettier

--- App.tsx
+++ App.tsx
@@ -1,37 +1,34 @@
-import * as React from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import List from "./src/List";
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import List from './src/List';
 
 function DetailsScreen() {
-	return (
-		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text>Details Screen</Text>
-		</View>
-	);
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+        </View>
+    );
 }
 
 export type RootStackParamList = {
-	Home: undefined;
-	Details: { id: string; };
-}
+    Home: undefined;
+    Details: { id: string };
+};
 const Stack = createStackNavigator<RootStackParamList>();
 
-function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen
-					name="Home"
-					options={{ title: "Overview" }}
-					>
-					{ props => <List {...props} /> }
-					</Stack.Screen>
-				<Stack.Screen name="Details" component={DetailsScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
-}
+const App = (): JSX.Element => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" options={{ title: 'Overview' }}>
+                    {(props) => <List {...props} />}
+                </Stack.Screen>
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
 
 export default App;
--- src/List/index.tsx
+++ src/List/index.tsx
@@ -1,37 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { RootStackParamList } from "../../App";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../App';
 
-type ProfileScreenNavigationProp = StackNavigationProp<
-	RootStackParamList,
-	"Home"
->;
+type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
 type Props = {
-	navigation: ProfileScreenNavigationProp;
+    navigation: ProfileScreenNavigationProp;
 };
 
-export default function App(props: Props) {
-	const { navigation } = props;
-	return (
-		<View style={styles.container}>
-			<StatusBar style="auto" />
-			<Text>Screen 1 : List Component</Text>
-			<Button
-				onPress={() => navigation.navigate("Details", { id: "1" })}
-				title="Go to details page"
-			/>
-		</View>
-	);
-}
+const List = (props: Props): JSX.Element => {
+    const { navigation } = props;
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Text>Screen 1 : List Component</Text>
+            <Button onPress={() => navigation.navigate('Details', { id: '1' })} title="Go to details page" />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+export default List
