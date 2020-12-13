7854116645a38b0313c669934b61688f44ee95f0
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2020-12-13 10:25:14 +0200

    Add react-native-elements and list characters with name and avatar.

--- src/List/list.tsx
+++ src/List/list.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../App';
-import { ChildDataProps } from './'
+import { ChildDataProps, Character } from './';
 
 type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
@@ -11,17 +13,39 @@ interface NavigationProp {
     navigation: ProfileScreenNavigationProp;
 }
 
-type Props = NavigationContainer & ChildDataProps;
+type Props = NavigationProp & ChildDataProps;
+
+interface RenderItemProps {
+    items: Character;
+    index: number;
+}
+
+const renderItem = (props: RenderItemProps & NavigationProp) => {
+    const { item, navigation } = props;
+    return (
+        <ListItem bottomDivider onPress={() => navigation.navigate('Details', { id: '1' })}>
+            <Avatar title={item.name} source={item.image && { uri: item.image }} />
+            <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron />
+        </ListItem>
+    );
+};
+
+const keyExtractor = (item, index) => index.toString();
 
 const List = (props: Props): JSX.Element => {
     const { navigation, data } = props;
-		console.log({data})
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
-            <Text>Screen 1 : List Component</Text>
-            <Button onPress={() => navigation.navigate('Details', { id: '1' })} title="Go to details page" />
-						{ data && data.characters && Array.isArray(data.characters.results) && data.characters.results.map( character => <Text>{ character.name }</Text>)}
+            <FlatList
+                style={styles.list}
+                keyExtractor={keyExtractor}
+                data={data && data.characters && data.characters.results}
+                renderItem={(character) => renderItem({ ...character, navigation })}
+            />
         </View>
     );
 };
@@ -29,10 +53,14 @@ const List = (props: Props): JSX.Element => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%',
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    list: {
+        width: '100%',
+    },
 });
 
-export default List
+export default List;
